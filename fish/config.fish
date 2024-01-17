bbe59175ccab6c8663351d45c83b829481432bf5
xwj31 <william@hove.capital>
2024-01-17 12:25:57 +0000

    add fish alias

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+# Commands to run in interactive sessions can go here
+alias yb "yarn build"
+alias ys "yarn start"
+alias yc "yarn clean"
+alias yd "yarn dev"
+alias bepi "bundle exec pod install --verbose"
+alias ut "yarn test -u"
+alias gs "git status"
+alias g- "git checkout -"
+alias gpl "git pull"
+alias gpu "git push"
+alias gst "git status"
+alias grb "git rebase origin/main"
+alias nk "npx npkill"
+alias ab "adb reverse tcp:8081 tcp:8081"
+alias yt "yarn test"
+rtx activate fish | source
+
+## some stuff
+ set PATH /usr/local/bin /usr/sbin $PATH
+
+## Homebrew
+
+"~/.config/fish/config.fish" 38L, 1140B
