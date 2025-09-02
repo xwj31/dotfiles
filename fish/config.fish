0ba5193ca728514ebe7fea85f79cb77f9a67dc30
Will <58257164+xwj31@users.noreply.github.com>
2025-09-02 16:19:41 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,44 @@
-# Commands to run in interactive sessions can go here
+# Fish Shell Configuration
+
+# ===========================
+# Homebrew (Apple Silicon)
+# ===========================
+eval (/opt/homebrew/bin/brew shellenv)
+
+# ===========================
+# Runtime Version Manager
+# ===========================
+mise activate fish | source
+
+# ===========================
+# Yarn/Node Aliases
+# ===========================
 alias yb "yarn build"
 alias ys "yarn start"
 alias yc "yarn clean"
 alias yd "yarn dev"
-alias bepi "bundle exec pod install --verbose"
+alias yt "yarn test"
 alias ut "yarn test -u"
+alias nk "npx npkill"
+
+# ===========================
+# Git Aliases
+# ===========================
 alias g- "git checkout -"
 alias gpl "git pull"
 alias gpu "git push"
 alias gst "git status"
 alias grb "git rebase origin/main"
-alias nk "npx npkill"
+alias gco "git checkout"
+alias gcm "git commit -m"
+alias ga "git add ."
+alias gd "git diff"
+
+# ===========================
+# Mobile Development
+# ===========================
+# iOS
+alias bepi "bundle exec pod install --verbose"
+
+# Android
 alias ab "adb reverse tcp:8081 tcp:8081"
-alias yt "yarn test"
-rtx activate fish | source
-
-## some stuff
- set PATH /usr/local/bin /usr/sbin $PATH
-
-## Homebrew
-
-"~/.config/fish/config.fish" 38L, 1140B
